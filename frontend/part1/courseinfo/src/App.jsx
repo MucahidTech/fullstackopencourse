9291706a5f06165b0dd4f11b4c729555e5c71a29
MucahidTech <mucahid.tech@gmail.com>
2026-05-19 09:04:52 +0300

    refactor: replace separate objects with single array - exercise 1.4

--- frontend/part1/courseinfo/src/App.jsx
+++ frontend/part1/courseinfo/src/App.jsx
@@ -13,41 +13,46 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part name={props.part1.name} exercises={props.part1.exercises} />
-      <Part name={props.part2.name} exercises={props.part2.exercises} />
-      <Part name={props.part3.name} exercises={props.part3.exercises} />
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
     </div>
   );
 };
 
 const Total = (props) => {
-  return <p> Number of exercises {props.totalExercises} </p>;
+  const total =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
+
+  return <p>Number of exercises {total}</p>;
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-
-  const totalExercises = part1.exercises + part2.exercises + part3.exercises;
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <>
       <Header course={course} />
 
-      <Content part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
 
-      <Total totalExercises={totalExercises} />
+      <Total parts={parts} />
     </>
   );
 };
