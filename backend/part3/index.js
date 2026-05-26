7327c27320e10c1a66fdbe6544a8d4c5010e823d
MucahidTech <mucahid.tech@gmail.com>
2026-05-26 21:58:59 +0300

    feat: add POST endpoint for new persons with random id generation - exercise 3.5

--- backend/part3/index.js
+++ backend/part3/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: "1",
@@ -44,6 +46,32 @@ app.get("/api/persons/:id", (request, response) => {
   } else {
     response.status(404).end();
   }
+});
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number || "000-0000",
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 app.delete("/api/persons/:id", (request, response) => {
