84b03aa2b0fa407e010efea14d4e6643b2d7d03c
MucahidTech <mucahid.tech@gmail.com>
2026-05-27 22:37:53 +0300

    feat: configure morgan to log POST request body - exercise 3.8

--- backend/part3/index.js
+++ backend/part3/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+morgan.token("body", (request) => JSON.stringify(request.body));
+
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
 
 let persons = [
   {
