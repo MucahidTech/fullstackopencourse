028254c5aa459beca43031a0118379853e1a888c
MucahidTech <mucahid.tech@gmail.com>
2026-06-09 00:14:46 +0300

    feat: add custom validator for phone number format - exercise 3.20

--- backend/part3/models/person.js
+++ backend/part3/models/person.js
@@ -22,7 +22,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d{5,}$/.test(v);
+      },
+      message: (props) =>
+        `${props.value} is not a valid phone number! Use format: XX-XXXXXXX or XXX-XXXXXXXX`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
