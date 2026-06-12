f71c7f073276a69c8ece7f969c4bfa34d78a2cf5
MucahidTech <mucahid.tech@gmail.com>
2026-06-12 07:48:24 +0300

    test(bloglist): add GET /api/blogs test with SuperTest and refactor to async/await - exercise 4.8

---
+++ part4/bloglist/tests/person_api.test.js
@@ -0,0 +1,26 @@
+const { test, describe, after } = require("node:test");
+const assert = require("node:assert");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const app = require("../app");
+
+const api = supertest(app);
+
+describe("Testing blogs API", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+
+    assert.strictEqual(response.body.length, 2);
+  });
+
+  after(async () => {
+    await mongoose.connection.close();
+  });
+});
