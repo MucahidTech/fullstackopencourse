3138e5c912dc11bbbb93a1c428c8959dd7eea925
MucahidTech <mucahid.tech@gmail.com>
2026-06-12 08:01:38 +0300

    test(bloglist): verify blog identifier is named 'id' instead of '_id' - exercise 4.9

--- part4/bloglist/tests/person_api.test.js
+++ part4/bloglist/tests/person_api.test.js
@@ -20,6 +20,23 @@ describe("Testing blogs API", () => {
     assert.strictEqual(response.body.length, 2);
   });
 
+  test.only("unique identifier is named 'id' not '_id'", async () => {
+    const newBlog = {
+      title: "Test Blog",
+      author: "Tester",
+      url: "http://test.com",
+      likes: 5,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(201);
+
+    const response = await api.get("/api/blogs");
+    const blog = response.body[2];
+
+    assert.ok(blog.id);
+    assert.strictEqual(blog._id, undefined);
+  });
+
   after(async () => {
     await mongoose.connection.close();
   });
