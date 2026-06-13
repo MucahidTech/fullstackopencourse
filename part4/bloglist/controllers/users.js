37d2ba158ab8ea01224011479dca73d90d41458d
MucahidTech <mucahid.tech@gmail.com>
2026-06-13 09:45:24 +0300

    feat(bloglist): implement user creation with bcryptjs password hashing - exercise 4.15

---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,35 @@
+const bcrypt = require("bcryptjs");
+const usersRouter = require("express").Router();
+const User = require("../models/user");
+
+usersRouter.post("/", async (request, response) => {
+  const { username, name, password } = request.body;
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
+});
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
+usersRouter.get("/:id", async (request, response) => {
+  const user = await User.findById(request.params.id);
+  if (user) {
+    response.json(user);
+  } else {
+    response.status(404).end();
+  }
+});
+
+module.exports = usersRouter;
