481e1a0176cbae116603c0eb441f13b0d9bef4f9
MucahidTech <mucahid.tech@gmail.com>
2026-06-15 15:17:57 +0300

    feat: persist login with localStorage and add logout - ex 5.2

--- part5/bloglist/bloglist-frontend/src/App.jsx
+++ part5/bloglist/bloglist-frontend/src/App.jsx
@@ -25,6 +25,7 @@ const App = () => {
 
     try {
       const user = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
       blogService.setToken(user.token);
       setUser(user);
       setUsername("");
@@ -35,6 +36,20 @@ const App = () => {
     }
   };
 
+  const handleLogOut = (event) => {
+    window.localStorage.removeItem("loggedBlogappUser");
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
@@ -42,7 +57,7 @@ const App = () => {
   const loginForm = () => {
     return (
       <>
-        <h2>Login</h2>
+        <h2>Log in to application</h2>
         <form onSubmit={handleLogin}>
           <div>
             <label>
@@ -73,8 +88,10 @@ const App = () => {
   const blogForm = () => {
     return (
       <>
-        <Notification message={notification} />
         <h2>blogs</h2>
+        <p>
+          {user.name} logged in <button onClick={handleLogOut}>logout</button>
+        </p>
         {blogs.map((blog) => (
           <Blog key={blog.id} blog={blog} />
         ))}
@@ -84,13 +101,9 @@ const App = () => {
 
   return (
     <div>
+      <Notification message={notification} />
       {!user && loginForm()}
-      {user && (
-        <div>
-          <p>{user.name} logged in</p>
-          {blogForm()}
-        </div>
-      )}
+      {user && <div>{blogForm()}</div>}
     </div>
   );
 };
