8c475d9216ba55298774cb48d9d39b62c9707359
MucahidTech <mucahid.tech@gmail.com>
2026-06-17 00:20:54 +0300

    feat: add toggleable blog details view - ex 5.7

--- part5/bloglist/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,25 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  return (
+    <div className="blogStyle">
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? "hide" : "view"}
+        </button>
+      </div>
+      {showDetails && (
+        <div>
+          <p>{blog.url}</p>
+          <p>Likes: {blog.likes}</p>
+          <p>{blog.user?.name || "Unknown author"}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
