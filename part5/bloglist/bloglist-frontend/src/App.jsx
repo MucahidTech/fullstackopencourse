c052eceded3efef487b4769a2b0c24b7bea04ed3
MucahidTech <mucahid.tech@gmail.com>
2026-06-16 23:35:58 +0300

    feat: toggle blog creation form visibility - ex 5.5

--- part5/bloglist/bloglist-frontend/src/App.jsx
+++ part5/bloglist/bloglist-frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 import LoginForm from "./components/LoginForm";
+import Togglable from "./components/Togglable";
 
 const App = () => {
   const [notification, setNotification] = useState(null);
@@ -129,7 +130,7 @@ const App = () => {
         <p>
           {user.name} logged in <button onClick={handleLogOut}>logout</button>
         </p>
-        {addForm()} <br />
+        <Togglable buttonLabel="Create New Blog">{addForm()}</Togglable>
         {blogs.map((blog) => (
           <Blog key={blog.id} blog={blog} />
         ))}
