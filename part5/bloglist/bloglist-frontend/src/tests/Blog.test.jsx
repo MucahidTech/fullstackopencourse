3639881113effa65f8b2c52b3c9540810a78f44e
MucahidTech <mucahid.tech@gmail.com>
2026-06-18 19:50:31 +0300

    test(frontend): verify blog shows url and likes after clicking view button - ex 5.14

--- part5/bloglist/bloglist-frontend/src/tests/Blog.test.jsx
+++ part5/bloglist/bloglist-frontend/src/tests/Blog.test.jsx
@@ -1,16 +1,20 @@
 import { render, screen } from "@testing-library/react";
 import { expect, test, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
+
 import Blog from "../components/Blog";
 
-test("renders content", () => {
-  const blog = {
-    user: "samir",
-    likes: 1,
-    author: "samir",
-    title: "Test Blog",
-    url: "http://test.com",
-  };
+const mockHandler = vi.fn();
 
+const blog = {
+  user: "samir",
+  likes: 1,
+  author: "samir",
+  title: "Test Blog",
+  url: "http://test.com",
+};
+
+test("renders title and author, but not url or likes by default", () => {
   render(<Blog blog={blog} />);
 
   const titleElement = screen.getByTestId("blog-title");
@@ -23,4 +27,15 @@ test("renders content", () => {
   const likesElement = screen.queryByTestId("blog-likes");
   expect(urlElement).toBeNull();
   expect(likesElement).toBeNull();
+});
+
+test("shows url and likes after clicking view button", async () => {
+  render(<Blog blog={blog} />);
+
+  const user = userEvent.setup();
+  const button = screen.getByText("view");
+  await user.click(button);
+
+  const titleElement = screen.getByTestId("blog-title");
+  const authorElement = screen.getByTestId("blog-author");
 });
