51b556651697d2b1ed67975732bec3e76f476714
MucahidTech <mucahid.tech@gmail.com>
2026-06-19 18:14:43 +0300

    test(e2e): add login tests for success and failure scenarios - ex 5.18

--- part5/bloglist-e2e-testing/tests/blog_app.spec.js
+++ part5/bloglist-e2e-testing/tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require("@playwright/test");
+const { loginWith } = require("./helper");
 
 describe("Blog app", () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("http://localhost:3003/api/testing/reset");
+    await request.post("http://localhost:3003/api/users", {
+      data: {
+        name: "Mucahid",
+        username: "firstUser",
+        password: "StrongPassword",
+      },
+    });
     await page.goto("http://localhost:5173");
   });
 
@@ -10,5 +19,23 @@ describe("Blog app", () => {
     await expect(page.getByLabel("username")).toBeVisible();
     await expect(page.getByLabel("password")).toBeVisible();
     await expect(page.getByRole("button", { name: "login" })).toBeVisible();
+  });
+
+  describe("Login", () => {
+    test("succeeds with correct credentials", async ({ page }) => {
+      await loginWith(page, "firstUser", "StrongPassword");
+      await expect(page.getByText("Login Successful")).toBeVisible();
+    });
+
+    test("fails with wrong credentials", async ({ page }) => {
+      await loginWith(page, "newUser", "wrongPassword");
+
+      const errorDiv = page.locator(".error");
+      await expect(errorDiv).toContainText("Wrong username or password");
+      await expect(errorDiv).toHaveCSS("border-style", "solid");
+      await expect(errorDiv).toHaveCSS("color", "rgb(255, 0, 0)");
+
+      await expect(page.getByText("Mucahid logged in")).not.toBeVisible();
+    });
   });
 });
