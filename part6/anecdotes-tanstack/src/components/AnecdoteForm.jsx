e1bbed149b65e3757d1596c08af09f8e8efbe098
MucahidTech <mucahid.tech@gmail.com>
2026-07-07 22:40:07 +0300

    feat: add error handling for short anecdotes with notification - ex 6.21

--- part6/anecdotes-tanstack/src/components/AnecdoteForm.jsx
+++ part6/anecdotes-tanstack/src/components/AnecdoteForm.jsx
@@ -9,6 +9,12 @@ const AnecdoteForm = () => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     event.target.reset();
+    if (content.length < 5) {
+      showNotification(
+        "too short anecdote, must be at least 5 characters long",
+      );
+      return;
+    }
     addAnecdoteToServer(content);
     showNotification(`anecdote '${content}' added`);
   };
